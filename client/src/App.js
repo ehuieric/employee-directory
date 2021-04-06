8fb926ace876d6cc61517e69bac658114b7f90c7
ehui eric <ehui_eric@yahoo.fr>
2021-04-06 14:26:24 -0400

    initialization

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import logo from './logo.svg';
+import './App.css';
+import "./index.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="ERIC">
+       
+       STAFF DIRECTORY 
+      </header>
+     
+    </div>
+  );
+}
+
+export default App;
